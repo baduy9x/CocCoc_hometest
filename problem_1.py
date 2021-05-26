2f0c9d0316c957b34a3f9e2d8fb8675959b66989
baduy9x <trinhvanduyhust@gmail.com>
2021-05-26 16:04:10 +0700

    add problem 1

---
+++ problem_1.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+sample = open('../../hash_catid_count.csv', 'r')
+freq_counter = Counter()
+total_counter = Counter()
+
+for line in sample.readlines():
+    object_id, cat_list, count_list = line.split("\t")
+    object_id = int(object_id)
+    cat_list = list(map(int, cat_list.strip()[1:-1].split(",")))
+    count_list = list(map(int, count_list.strip()[1:-1].split(",")))
+    for i, j in zip(cat_list, count_list):
+        freq_counter[i] += 1
+        total_counter[i] += j
+
+print(freq_counter.most_common(1))
+print(total_counter.most_common(1))
